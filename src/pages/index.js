194a98c5a514d272b46a47c95b0553b0d3b76390
bekhruzxon2010 <najmiddinovb894@gmail.com>
2025-02-25 17:34:25 +0500

    dd

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Sigin from "./sign-in";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +23,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div>
+        <Sigin/>
+      </div>
     </>
   );
 }
