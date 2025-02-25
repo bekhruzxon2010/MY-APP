631401430ecd6d4ac3d234a8f3bb9e6b568014de
bekhruzxon2010 <najmiddinovb894@gmail.com>
2025-02-25 17:42:59 +0500

    aloo

--- src/component/NowCard.js
+++ src/component/NowCard.js
@@ -1,52 +1,52 @@
 import React from 'react'
-import '../styles/NowCard.module.css'
+import styles from '../styles/NowCard.module.css'
 import Image from "next/image"
 
 
 const NowCard = () => {
     return (
         <>
-            <div className='mleft1'>
-                <div className='dfc'>
-                    <div className='mleftww'>
+            <div className={styles['mleft1']}>
+                <div className={styles['dfc']}>
+                    <div className={styles['mleftww']}>
                         <h2>Get Started Now</h2>
-                        <div className="input_name">
-                            <p className='text_name'>Name</p>
-                            <input className='input_own' type="text" placeholder='Enter your name' />
+                        <div className={styles['input_name']}>
+                            <p className={styles['text_name']}>Name</p>
+                            <input className={styles['input_own']}type="text" placeholder='Enter your name' />
                         </div>
-                        <div className="input_name">
-                            <p className='text_name'>Email address</p>
-                            <input className='input_own' type="email" placeholder='Enter your e-mail' />
+                        <div className= {styles['input_name']}>
+                            <p className={styles['text_name']}>Email address</p>
+                            <input className={styles['input_own']} type="email" placeholder='Enter your e-mail' />
                         </div>
-                        <div className="input_name">
-                            <p className='text_name'>Password</p>
-                            <input className='input_own' type="password" placeholder='Enter your password' />
+                        <div className={styles['input_name']}>
+                            <p className={styles['text_name']}>Password</p>
+                            <input className={styles['input_own']} type="password" placeholder='Enter your password' />
                         </div>
-                        <div className="df">
-                            <input className='check' type="checkbox" />
-                            <p className='agree'>I agree to the terms & policy</p>
+                        <div className={styles['df']}>
+                            <input className={styles['check']} type="checkbox" />
+                            <p className={styles['agree']}>I agree to the terms & policy</p>
                         </div>
                         <div>
-                            <button className='btn_style'>Signin</button>
-                            <hr className='hr' />
+                            <button className={styles['btn_style']}>Signin</button>
+                            <hr className={styles['hr']} />
                         </div>
                     </div>
-                    <div className="all">
-                        <div className="google">
-                            <Image className='mtop' src="/google.png" width={19} height={19} alt="Google Logo" />
-                            <p className='mleft'>Sign in with Google</p>
+                    <div className={styles['all']}>
+                        <div className={styles['google']}>
+                            <Image className={styles['mtop']} src="/google.png" width={19} height={19} alt="Google Logo" />
+                            <p className={styles['mleft']}>Sign in with Google</p>
                         </div>
-                        <div className="apple">
-                            <Image className='mtop' src="/apple.png" width={19} height={19} alt="Apple Logo" />
-                            <p className='mleft'>Sign in with Apple</p>
+                        <div className={styles['apple']}>
+                            <Image className={styles['mtop']} src="/apple.png" width={19} height={19} alt="Apple Logo" />
+                            <p className={styles['mleft']}>Sign in with Apple</p>
                         </div>
                     </div>
                     <div>
-                        <p className='center'>Have an account? Sign In</p>
+                        <p className={styles['center']}>Have an account? Sign In</p>
                     </div>
                 </div>
                 <div>
-                    <img src='back.png' className='mleft_img' alt="Background" width={'350px'}/>
+                    <img src='back.png' className={styles['mleft_img']} alt="Background" width={'350px'}/>
                 </div>
             </div>
         </>
